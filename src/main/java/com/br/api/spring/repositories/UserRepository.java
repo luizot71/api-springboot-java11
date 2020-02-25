88c9d8433ee4ebd245c2093951f985abb7be71cc
Luiz Otavio <luizotaviomartinsf@gmail.com>
2020-02-25 18:48:54 -0300

    Inclusão da camada de serviço, add Service layer, component registration

--- src/main/java/com/br/api/spring/repositories/UserRepository.java
+++ src/main/java/com/br/api/spring/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package com.br.api.spring.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.br.api.spring.entities.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long>{
 
 }
